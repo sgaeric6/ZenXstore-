143a2b0cdb12906a9a11ea65225051f9f57964dd
sgaeric6 <sgaeric6@gmail.com>
2026-08-09 10:13:35 +0100

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+export default function Loading() {
+  return (
+    <main className="loadingPage">
+      <div className="loadingLogo">
+        Z
+      </div>
+
+      <div className="loadingBar" />
+
+      <p>Loading ZenXStore...</p>
+    </main>
+  );
+}
