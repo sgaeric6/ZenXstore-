709758bebd34af3d04edb7691489d55cfdce705e
sgaeric6 <sgaeric6@gmail.com>
2026-08-09 09:34:46 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import MarketplacePreview from "../components/MarketplacePreview";
+import Features from "../components/Features";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <MarketplacePreview />
+      <Features />
+    </>
+  );
+}
